1e5ff6fa910b7804669fc0582e7ee62b3ce8f365
GKCY <frendchen1997@gmail.com>
2016-10-31 12:45:24 +0800

    update

--- hello.py
+++ hello.py
@@ -1,19 +1,35 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment 
+from flask_wtf import Form 
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class NameForm(Form):
+	name = StringField('What is your name?',validators=[Required()])
+	submit = SubmitField('Submit')
+
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html',
-							current_time=datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Look like you have changed your name')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -22,10 +38,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
-
-# @app.route('/')
-# def index():
-# 	return render_template('index.html')
 
 @app.route('/<name>')
 def user(name):
